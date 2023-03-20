86b90e72da1df8b276bbeac1791db79530e2f493
L1nusWahl <linus.wahl@elev.ga.ntig.se>
2023-03-20 14:52:16 +0100

    Vecka 11

--- java/script.js
+++ java/script.js
@@ -1,13 +1,13 @@
-var images = ["img/friidrott1.jpg", "img/relay.jpg", "img/100m.jpg", "img/hurdle.jpg"]; // add the paths of the images you want to show in the array
-var currentIndex = 0; // initialize the index of the current image to 0
+var images = ["img/friidrott1.jpg", "img/relay.jpg", "img/100m.jpg", "img/hurdle.jpg"]; 
+var currentIndex = 0; 
 
 
 function nextImage() {
-  currentIndex = (currentIndex + 1) % images.length; // update the index of the current image
-  document.getElementById("slideshow").src = images[currentIndex]; // set the source of the image to the next image in the array
+  currentIndex = (currentIndex + 1) % images.length;
+  document.getElementById("slideshow").src = images[currentIndex]; 
 } 
 
-setInterval(nextImage, 5000); // call the nextImage function every 3 seconds to switch between the images
+setInterval(nextImage, 5000); 
 
 const burger = document.getElementById('burger')
 const nav = document.getElementById('nav-links')
